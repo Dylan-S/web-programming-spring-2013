37841dfb7141cceddc5319935e4e5a7c15293114
Dylan <hobogrunt@excite.com>
2013-01-30 16:39:30 -0800

    added lesson 2

---
+++ lesson-02/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Home</title>
+</head>
+
+<body>
+<p>I'm Dylan Stefanisko, I'm taking this class because it's required for my major, but I'm also interested in learning database driven webpages because it sounds like a very useful and powerful skill to have. I am <?php
+$age = 10 + 9;
+echo $age;
+?> years old. </p>
+</body>
+</html>
